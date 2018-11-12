0e3f5854751ee5ac77b321c4cfd407550cd4e7a1
Khalid Elassaad <khalidelassaad@gmail.com>
2018-11-12 11:23:28 -0800

    initial plan

---
+++ gfycatsplitter.py
@@ -0,0 +1,14 @@
+"""
+The goal of this file is to learn how to do the following things with reddit and gfycat
+1. Download a gfycat file via Reddit API
+    - Reddit API searches by site and downloads X number of results (by date)
+    - "gfycat" package from pip
+2. Edit the linked file (crossview -> parallel view transformation)
+    - Slice along vertical axis and swap left and right data partitions
+3. Upload to gfycat.com and save sharing URL
+4. Post the URL to reddit
+
+I am doing this in its own file rather than adding to the old bot because I want to try building the process my own way
+After verifying my solution works, I will merge it with CrossXParellelBot.py
+"""
+
